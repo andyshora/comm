600250a29227c8ab20d6396bf79318d54305d5d0
Andy Shora <andyshora@gmail.com>
2016-11-05 10:54:14 +0000

    Cones moving.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,11 +10,15 @@ const shaderVert = glslify('./../shaders/custom.vert');
 const shaderFrag = glslify('./../shaders/custom.frag');
 
 const CUBE_WIDTH = 2;
+const CONE_RADIUS = 2;
 const NUM_CUBES = 200;
+const MAX_TIMER = 1000;
+const SPEED = 10;
 const DIMS = {
   x: { min: -500, length: 1000 },
   y: { min: -500, length: 1000 }
 };
+let timer = 0;
 
 const c = new Chance();
 
@@ -25,27 +29,64 @@ class Main extends AbstractApplication {
 
     this._controls.enableZoom = true;
     this._camera.position.z = 700;
-    this._boxes = [];
-
-    var texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
-
-    var geometry = new THREE.BoxGeometry( CUBE_WIDTH, CUBE_WIDTH, CUBE_WIDTH );
-    var material = new THREE.MeshBasicMaterial( { map: texture } );
-
-    var material2 = new THREE.ShaderMaterial({
-        vertexShader: shaderVert,
-        fragmentShader: shaderFrag
-    });
+    this._cubes = [];
+    this._cones = [];
 
     const light = new THREE.PointLight(0xFFFFFF, 1);
     light.position.copy(this._camera.position);
     this._scene.add(light);
 
-    this._cubes = [];
+    this._addCubes();
+    this._addCones();
+
+    this.animate();
+
+  }
+
+  _addCones() {
     const thetaSlice = 360 / NUM_CUBES;
 
+    var geometry = new THREE.ConeGeometry( CONE_RADIUS, CONE_RADIUS * 10, 8 );
+    var material = new THREE.ShaderMaterial({
+      vertexShader: shaderVert,
+      fragmentShader: shaderFrag
+    });
+
     for (let i = 0; i < NUM_CUBES; i++) {
-      const mesh = new THREE.Mesh( geometry, material2 );
+      const mesh = new THREE.Mesh( geometry, material );
+
+      const theta = thetaSlice * i;
+      const p = Trig.getRadialPosition({ angle: theta, radius: DIMS.x.length * 0.38 });
+
+      mesh.position.x = p.x;
+      mesh.position.y = p.y;
+      mesh.position.z = 0;
+
+      mesh.rotation.z = Trig.d2r(360 - (theta + 90));
+
+      this._cones.push({
+        i,
+        mesh,
+        name: chance.name(),
+        position: mesh.position,
+        theta
+      });
+
+      this._scene.add( mesh );
+    }
+  }
+
+  _addCubes() {
+    const thetaSlice = 360 / NUM_CUBES;
+
+    var geometry = new THREE.CubeGeometry( CUBE_WIDTH, CUBE_WIDTH, CUBE_WIDTH );
+    var material = new THREE.ShaderMaterial({
+        vertexShader: shaderVert,
+        fragmentShader: shaderFrag
+    });
+
+    for (let i = 0; i < NUM_CUBES; i++) {
+      const mesh = new THREE.Mesh( geometry, material );
 
       const theta = thetaSlice * i;
       const p = Trig.getRadialPosition({ angle: theta, radius: DIMS.x.length * 0.4 });
@@ -54,23 +95,52 @@ class Main extends AbstractApplication {
       mesh.position.y = p.y;
       mesh.position.z = 0;
 
-      this._boxes.push({
-        position: mesh.position,
-        theta,
-        mesh,
+      this._cubes.push({
         i,
-        name: chance.name()
+        mesh,
+        name: chance.name(),
+        position: mesh.position,
+        theta
       });
 
-      this._cubes.push(mesh);
-       this._scene.add( mesh );
+      this._scene.add( mesh );
     }
-
-    console.log(this._boxes);
-
-    this.animate();
-
   }
+
+  getCubePosition(i) {
+    return this._cubes[i].position;
+  }
+
+  getRandomCube() {
+    const index = chance.integer({min: 0, max: this._cubes.length - 1}); 
+    return this._cubes[index];
+  }
+
+  animate(data) {
+    super.animate();
+    // console.log(timer);
+
+    const progress = timer / MAX_TIMER;
+
+    for (let i = 0; i < this._cones.length; i++) {
+      const cone = this._cones[i];
+
+      const radius = 0.4 * DIMS.x.length - (progress * 0.4 * DIMS.x.length);
+
+      const p = Trig.getRadialPosition({ angle: cone.theta, radius: radius });
+
+      this._cones[i].mesh.position.y = p.x;
+      this._cones[i].mesh.position.x = p.y;
+    }
+    
+    this._renderer.render(this._scene, this._camera);
+    timer += SPEED;
+
+    if (timer > MAX_TIMER) {
+      timer = 0;
+    }
+  }
+
 
 }
 export default Main;
--- src/shaders/custom.frag
+++ src/shaders/custom.frag
@@ -5,6 +5,6 @@ void main() {
   float brightness = noise(gl_FragCoord.xx);
 
     //gl_FragColor = vec4(vec3(brightness), 1.0);
-    gl_FragColor = vec4(vec3(1.0,0.0,0.0), 0.0);
+    gl_FragColor = vec4(vec3(1.0,1.0,0.0), 0.0);
 
 }
